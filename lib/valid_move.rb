289977b0a085783599792b0e4b69f9bb07a2437d
Arti M <artismarti@gmail.com>
2018-09-09 21:58:16 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -10,7 +10,7 @@ end
 def position_taken?(board, index)
   if (board[index] == "" || board[index] == " " || board[index] == nil) 
     false
-    elsif (board[index] == "X" || board[index] == "O")
+  elsif (board[index] == "X" || board[index] == "O")
     true
   end
 end
